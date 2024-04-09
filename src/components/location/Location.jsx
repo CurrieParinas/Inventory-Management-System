6dbf17089aaa908772e7d76f8ab9d6a3602cb577
rsgomez1 <rsgomez1@up.edu.ph>
2024-04-09 21:38:41 +0800

    Connection for Recent Activity in Location

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./location.scss"
 import locIcon from "../../assets/location.svg"
 import { locationDummy } from '../../sidebar'
 
 const Location = () => {
+    const[locations, setLocations]=useState([])
+
+    const fetchLocations = async () => {
+        try {
+          const response = await fetch('http://localhost:8080/inventory/location/fiveLastModified');
+          if (!response.ok) {
+            throw new Error('Failed to fetch locations');
+          }
+          const data = await response.json();
+          setLocations(data);
+        } catch (error) {
+          console.error('Error fetching locations:', error);
+        }
+      };
+
+    useEffect(() => {
+    fetchLocations();
+    }, []);
+
   return (
     <div className='location'>
         <div className="somediv">
@@ -11,16 +30,16 @@ const Location = () => {
             <h1>Location</h1>
         </div>
         <div className="list">
-            {locationDummy.map(loc=>(
-                <div className="listItem" key={loc.id}>
+            {locations.map(loc=>(
+                <div className="listItem" key={loc.LOCATION_ID}>
                     <div className="locItemInfo">
-                        <div className="itemNumber" style={{minWidth: '16.5px'}}>{loc.id}</div>
+                        <div className="itemNumber" style={{minWidth: '16.5px'}}>{loc.LOCATION_ID}</div>
                         <div className="locationTexts">
-                            <span className="trackedItemName">{loc.locationName} </span>
+                            <span className="trackedItemName">{loc.NAME} </span>
                             
                         </div>
                     </div>
-                    <span className="category"><i>{loc.parentLocation} </i></span>
+                    <span className="category"><i>{loc.PARENT_LOCATION} </i></span>
                 </div>
             ))}
         </div>
